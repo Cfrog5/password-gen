85b0d15c1d0e145dd2b26aaee65cc58e05a77b6b
Cfrog5 <cjfraga05@gmail.com>
2023-08-24 22:02:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## Password-Generator
+
+## Description
+
+This is to assist users with creating a random and secure password easily and quickly. 
+
+## Usage 
+
+To acces the application functions you first click on "generate password" which will then prompt you for the specifications for the password. 
+This is ranges from capital, lowercase, numbers, special characters, and length. 
+
+## Created 
+
+Chris Fraga
+
+![Alt text](image.png)
+
+## License
+
+[MIT License](https://www.mit.edu/~amini/LICENSE.md)
+
+
---
+++ script.js
@@ -0,0 +1,66 @@
+
+var generateBtn = document.querySelector("#generate");
+
+// Creates password for the input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Event listener to generate button.
+generateBtn.addEventListener("click", writePassword);
+
+// This will prompt a window to ask the user what the password requirments are.
+function generatePassword() {
+var charLength = window.prompt ("How many characters would you like in your password?");
+console.log (charLength);
+
+var upperCase = window.confirm ("Would you like uppercase characters in your password?");
+console.log (upperCase);
+
+var lowerCase = window.confirm ("Would you like lowercase characters in your password?");
+console.log (lowerCase);
+
+var numbers = window.confirm ("Would you like numbers in your password?");
+console.log (numbers);
+
+var specialChar = window.confirm ("Would you like special characters in your password?");
+console.log (specialChar);
+
+//if statements for window selections.
+if (lowerCase) {
+  selectedArray = selectedArray.concat(lowerCasedCharacters);
+}
+if (upperCase) {
+  selectedArray = selectedArray.concat(upperCasedCharacters);
+}
+if (numbers) {
+  selectedArray = selectedArray.concat(numericCharacters);
+}
+if (specialChar) {
+  selectedArray = selectedArray.concat(specialCharacters);
+}
+
+var passwordString = "";
+
+// Allows the gerator to select random charaters.
+for (var i = 0; i < charLength; i++) {
+  passwordString += selectedArray[Math.floor(Math.random() * (selectedArray.length))];
+}
+
+return passwordString;
+}
+
+// The Different characters that can be selected
+var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+
+var selectedArray = [];
